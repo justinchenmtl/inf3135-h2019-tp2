b89431dd112c6cfeb486e41b8453f0d666db613f
justinchenmtl <justinchenmtl@gmail.com>
2019-03-15 13:27:23 -0400

    modification de outils.c

--- outils.c
+++ outils.c
@@ -13,7 +13,6 @@
 int echange(long long *c, long long *d)
 {
 	long long temp;
-
 	if(*c > *d)
 	{
 		temp = *c;
@@ -25,43 +24,37 @@ int echange(long long *c, long long *d)
 
 // Fonction de calculer les puissances
 long long indice( long long bas, long long puiss) {
-  long long resultat = 1;
-  for(long long k = 0 ; k < puiss ; k++)
-    resultat *= bas;
-  return resultat;
+    long long resultat = 1;
+    for(long long k = 0 ; k < puiss ; k++){resultat *= bas;}
+	return resultat;
 }
  
 // Fonction de trouver les nombre parfaits entre deux intervalles 
 int nbParfait(long long debut, long long fin)
 {
-  bool flag = true;
-  long long p = EXPO;
-  long long nb;
+    bool flag = true;
+    long long p = EXPO;
+    long long nb;
 
-  if(debut <= 6 && fin >= 6){
-	  printf("6\n");
-  }
+    if(debut <= 6 && fin >= 6){printf("6\n");}
  
-  for(long long i = 3; i <= p; i+=1)
-  {
-    flag = true;
-    long long carre = (indice(2, i)-1);
-    for(long long j = 2; j*j < carre; j++)
+    for(long long i = 3; i <= p; i+=1)
     {
-      if( (carre % j == 0) )
-      {
-        flag = false;
-        break;
-      }
+        flag = true;
+        long long carre = (indice(2, i)-1);
+        for(long long j = 2; j*j < carre; j++)
+        {
+            if( (carre % j == 0) ){
+            flag = false;
+            break;
+            }
+        }
+        if(flag)
+        {
+            nb = indice(2, i-1)*(indice(2,i)-1);
+	        if(nb >= debut && nb <= fin){printf("%lld\n", nb);}
+	    }
     }
-    if(flag)
-    {
-      nb = indice(2, i-1)*(indice(2,i)-1);
-	  if(nb >= debut && nb <= fin){
-          printf("%lld\n", nb);  
-      }
-	}
-  }
   return 0;
 }
 
